253706008463653ef6628a8c5ffc337bd0c12a7d
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2021-11-16 20:29:58 +0300

    most of work done,
    need to fix mobile view and write JavaScript for button

---
+++ js/script.js
@@ -0,0 +1,9 @@
+
+
+var clickButton = document.getElementsByClassName('footer-link');
+console.log(clickButton[0]);
+    document.querySelector(".links").ClassList.remove(".display-none");
+// clickButton.addEventListener("click", function(){
+//     // document.getElementsByClassName('.links').ClassList.toggle('.display-none');
+//     console.log("button clicked");
+// });
