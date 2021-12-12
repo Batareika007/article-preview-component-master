4cd8c8620c0a3f7f5b26d7ab3e89f9fc67374de2
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2021-12-12 15:27:33 +0300

    config js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
 var clickButton = document.querySelector(".footer-link");
 var flowLinks = document.querySelector(".links");
+
 clickButton.addEventListener("click", function () {
     flowLinks.classList.toggle("display-none");
     document.querySelector(".footer-link").classList.toggle("footer-link-active");
-    console.log("button clicked");
+    // changeImage();
+    // console.log("button clicked");
+    function changeImage() {
+
+        // if (document.getElementById("changeOnClick").src == "images/icon-share.svg") {
+        //     document.getElementById("changeOnClick").src = "images/icon-share-white.svg";
+        //     console.log("change to white")
+        // } else {
+        //     document.getElementById("share-icon-on-click").src = "images/icon-share.svg";
+        //     console.log("change to regular")
+        // }
+        if (document.querySelector(".changeonclick").src == "images/icon-share.svg") {
+            document.querySelector(".changeonclick").src = "images/icon-share-white.svg";
+            console.log("change to white")
+        } else {
+            document.querySelector(".changeonclick").src = "images/icon-share.svg";
+            console.log("change to regular")
+        }
+    }
 });
