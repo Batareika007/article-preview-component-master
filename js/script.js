f3042f4098d272692496be29e0b3af95eeecc51c
Batareika007 <50496651+Batareika007@users.noreply.github.com>
2022-01-02 17:06:02 +0300

    fixed svg image on click
    project done

--- js/script.js
+++ js/script.js
@@ -4,17 +4,5 @@ var flowLinks = document.querySelector(".links");
 clickButton.addEventListener("click", function () {
     flowLinks.classList.toggle("display-none");
     document.querySelector(".footer-link").classList.toggle("footer-link-active");
-    // changeImage();
-    // console.log("button clicked");
-    function changeImage() {
-
-        // if (document.getElementById("changeOnClick").src == "images/icon-share.svg") {
-        //     document.getElementById("changeOnClick").src = "images/icon-share-white.svg";
-        //     console.log("change to white")
-        // } else {
-        //     document.getElementById("share-icon-on-click").src = "images/icon-share.svg";
-        //     console.log("change to regular")
-        // }
-      
-    }
+    console.log("button clicked");
 });
